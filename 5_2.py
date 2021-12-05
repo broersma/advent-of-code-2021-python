72d9cd074e6073d412546b3aa19c3f5be8c7027f
Eric Broersma <git@ericbroersma.nl>
2021-12-05 13:37:41 +0100

    Make day 5 a little nicer

--- 5_2.py
+++ 5_2.py
@@ -2,24 +2,24 @@ import little_helper
 from collections import defaultdict
 from itertools import zip_longest
 
+
+def range2(a, b):
+    if b > a:
+        yield from range(a, b + 1)
+    else:
+        yield from range(a, b - 1, -1)
+
+
 input = little_helper.get_input(5, 2021).split("\n")
 input = (tuple(tuple(int(coordinate) for coordinate in component.split(",")) for component in line.split(" -> ")) for line in input)
 
 diagram = defaultdict(int)
 
 for line in input:
-    xmin = min(line[0][0], line[1][0])
-    xmax = max(line[0][0], line[1][0])
-    ymin = min(line[0][1], line[1][1])
-    ymax = max(line[0][1], line[1][1])
-    
-    stepx = -1 if line[0][0] > line[1][0] else 1
-    xs = range(line[0][0], line[1][0] + stepx, stepx)
+    xs = list(range2(line[0][0], line[1][0]))
+    ys = list(range2(line[0][1], line[1][1]))
 
-    stepy =-1 if line[0][1] > line[1][1] else 1
-    ys = range(line[0][1], line[1][1] + stepy, stepy)
-
-    for (x,y) in zip_longest(xs, ys, fillvalue=xmin if xmin==xmax else ymin):
+    for (x,y) in zip_longest(xs, ys, fillvalue=xs[0] if len(xs) == 1 else ys[0]):
         diagram[(x, y)] += 1
 
 print(len([value for value in diagram.values() if value > 1]))
