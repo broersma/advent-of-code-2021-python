bad1b4ece35fc0bd7474b58736a2bd1f143bc365
Eric Broersma <git@ericbroersma.nl>
2021-12-07 12:22:49 +0100

    Better start from the average position :)

--- 7_2.py
+++ 7_2.py
@@ -4,7 +4,7 @@ from more_itertools import interleave
 input = list(map(int, little_helper.get_input(7, 2021).split(",")))
 
 fuel_usage = 99999999999999999
-center_pos = (max(input) + min(input)) // 2
+center_pos = sum(input) // len(input)
 for target_position in interleave(range(center_pos, max(input)), range(center_pos-1, min(input), -1)):
     fuel_usage = min(fuel_usage, sum(sum(range(abs(pos - target_position)+1)) for pos in input))
     print(fuel_usage, flush=True)
